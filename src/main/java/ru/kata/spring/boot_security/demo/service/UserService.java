c0048c052bd009c0fcb9b496485e11c684a41854
VBocharov87 <168235768+VBocharov87@users.noreply.github.com>
2024-08-28 23:48:12 +0300

    Add files via upload

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,17 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import ru.kata.spring.boot_security.demo.entities.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+
+    void saveUser(User user);
+
+    List<User> getAllUsers();
+
+    void updateUser(User user);
+
+    void deleteUser(Long id);
+}
