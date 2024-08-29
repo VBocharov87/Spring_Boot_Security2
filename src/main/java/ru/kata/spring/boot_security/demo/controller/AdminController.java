8b3ae2503f99ff55d264c52606e8ac8d28ac81b9
VBocharov87 <168235768+VBocharov87@users.noreply.github.com>
2024-08-29 17:04:07 +0300

    Add files via upload

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,66 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.entities.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin_page")
+public class AdminController {
+
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public AdminController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public String getAllUsersAndRoles(Model model) {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) auth.getPrincipal();
+        model.addAttribute("user", user);
+
+        List<User> users = userService.getAllUsers();
+        model.addAttribute("users", users);
+
+        model.addAttribute("roles", roleService.getAllRoles());
+        return "/admin_page";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute("user") User user,
+                           @RequestParam(required = false) List<Long> roles) {
+
+        roleService.setUserRoles(user, roles);
+        userService.saveUser(user, roles);
+        return "redirect:/admin_page";
+    }
+
+    @PostMapping("/updateUser")
+    public String updateUser(@ModelAttribute("user") User user,
+                           @RequestParam(required = false) List<Long> roles) {
+
+        roleService.setUserRoles(user, roles);
+        userService.updateUser(user, roles);
+        return "redirect:/admin_page";
+    }
+
+    @PostMapping("/delete")
+    public String deleteUser(@RequestParam("id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/admin_page";
+    }
+}
